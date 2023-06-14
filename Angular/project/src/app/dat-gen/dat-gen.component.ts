ae1af84f42dac3e40e635ccca14031013f9340b5
Anderson Leal Velez <andersonlealv@outlook.com>
2023-06-13 21:04:57 -0500

    Adecuación pestaña loc y arreglod e las listas, avance hasta la pestaña fisica

--- Angular/project/src/app/dat-gen/dat-gen.component.ts
+++ Angular/project/src/app/dat-gen/dat-gen.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { disableDebugTools } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dat-gen',
   templateUrl: './dat-gen.component.html',
-  styleUrls: ['./dat-gen.component.css']
+  styleUrls: ['./dat-gen.component.css'],
 })
 export class DatGenComponent {
+  constructor(private fb: FormBuilder) {}
 
+  get idoferta() {
+    return this.formUserGen.get('idoferta') as FormControl;
+  }
+
+  get dertip() {
+    return this.formUserGen.get('dertip') as FormControl;
+  }
+
+  get tipinmb() {
+    return this.formUserGen.get('tipinmb') as FormControl;
+  }
+
+  get valanex() {
+    return this.formUserGen.get('valanex') as FormControl;
+  }
+
+  get feccapofer() {
+    return this.formUserGen.get('feccapofer') as FormControl;
+  }
+
+  get tiemofemer() {
+    return this.formUserGen.get('tiemofemer') as FormControl;
+  }
+
+  get proinmb() {
+    return this.formUserGen.get('proinmb') as FormControl;
+  }
+
+  get desproy() {
+    return this.formUserGen.get('desproy') as FormControl;
+  }
+
+  formUserGen = this.fb.group({
+    idoferta: [{ value: '123456789', disabled: true }, Validators.required],
+    dertip: ['', Validators.required],
+    tipinmb: ['', Validators.required],
+    valanex: ['', Validators.required],
+    feccapofer: ['', Validators.required],
+    tiemofemer: ['', Validators.required],
+    proinmb: ['', Validators.required],
+    desproy: ['', [Validators.required, Validators.maxLength(500)]],
+  });
+
+  procesar() {
+    console.log(this.formUserGen.value);
+  }
 }
--- Angular/project/src/app/inf-fis/inf-fis.component.ts
+++ Angular/project/src/app/inf-fis/inf-fis.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { disableDebugTools } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-inf-fis',
   templateUrl: './inf-fis.component.html',
-  styleUrls: ['./inf-fis.component.css']
+  styleUrls: ['./inf-fis.component.css'],
 })
 export class InfFisComponent {
+  constructor(private fb: FormBuilder) {}
 
+  get idoferta() {
+    return this.formUserFis.get('idoferta') as FormControl;
+  }
+
+  get atm2() {
+    return this.formUserFis.get('atm2') as FormControl;
+  }
+
+  get atha() {
+    return this.formUserFis.get('atha') as FormControl;
+  }
+
+  formUserFis = this.fb.group({
+    idoferta: [{ value: '123456789', disabled: true }, Validators.required],
+    atm2: [
+      '',
+      [Validators.required, Validators.max(1000000), Validators.min(0)],
+    ],
+    atha: [
+      '',
+      [Validators.required, Validators.max(1000000), Validators.min(0)],
+    ],
+  });
+
+  procesar() {
+    console.log(this.formUserFis.value);
+  }
 }
