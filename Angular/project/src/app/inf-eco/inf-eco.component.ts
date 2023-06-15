ef5e2de1c83e8667c3098736466442df81b4caa4
Anderson Leal Velez <andersonlealv@outlook.com>
2023-06-14 21:26:28 -0500

    Vistas con totalidad de validaciones por campo no entre vistas

--- Angular/project/src/app/inf-eco/inf-eco.component.ts
+++ Angular/project/src/app/inf-eco/inf-eco.component.ts
@@ -1,10 +1,203 @@
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { disableDebugTools } from '@angular/platform-browser';
 @Component({
   selector: 'app-inf-eco',
   templateUrl: './inf-eco.component.html',
-  styleUrls: ['./inf-eco.component.css']
+  styleUrls: ['./inf-eco.component.css'],
 })
 export class InfEcoComponent {
+  constructor(private fb: FormBuilder) {}
 
+  get idoferta() {
+    return this.formUserEco.get('idoferta') as FormControl;
+  }
+
+  get valofeini() {
+    return this.formUserEco.get('valofeini') as FormControl;
+  }
+
+  get porneg() {
+    return this.formUserEco.get('porneg') as FormControl;
+  }
+
+  get valofefin() {
+    return this.formUserEco.get('valofefin') as FormControl;
+  }
+
+  get valterr() {
+    return this.formUserEco.get('valterr') as FormControl;
+  }
+
+  get valcm2() {
+    return this.formUserEco.get('valcm2') as FormControl;
+  }
+
+  get valap() {
+    return this.formUserEco.get('valap') as FormControl;
+  }
+
+  get valcul() {
+    return this.formUserEco.get('valcul') as FormControl;
+  }
+
+  get valavacat() {
+    return this.formUserEco.get('valavacat') as FormControl;
+  }
+
+  get valadmin() {
+    return this.formUserEco.get('valadmin') as FormControl;
+  }
+
+  get valarrini() {
+    return this.formUserEco.get('valarrini') as FormControl;
+  }
+
+  get valarrfin() {
+    return this.formUserEco.get('valarrfin') as FormControl;
+  }
+
+  get valtbp() {
+    return this.formUserEco.get('valtbp') as FormControl;
+  }
+
+  get valgar() {
+    return this.formUserEco.get('valgar') as FormControl;
+  }
+
+  get valdep() {
+    return this.formUserEco.get('valdep') as FormControl;
+  }
+
+  get valanex() {
+    return this.formUserEco.get('valanex') as FormControl;
+  }
+
+  formUserEco = this.fb.group({
+    idoferta: [{ value: '123456789', disabled: true }, Validators.required],
+    valofeini: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    porneg: ['', [Validators.required, Validators.max(100), Validators.min(0)]],
+    valofefin: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valterr: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valcm2: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valap: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valcul: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valavacat: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valadmin: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valarrini: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valarrfin: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+
+    valtbp: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+
+    valgar: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valdep: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+    valanex: [
+      '',
+      [
+        Validators.required,
+        Validators.max(10000000000000000),
+        Validators.min(0.000000000000000000000000001),
+      ],
+    ],
+  });
+
+  procesar() {
+    console.log(this.formUserEco.value);
+  }
 }
--- Angular/project/src/app/inf-fue/inf-fue.component.ts
+++ Angular/project/src/app/inf-fue/inf-fue.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { disableDebugTools } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-inf-fue',
   templateUrl: './inf-fue.component.html',
-  styleUrls: ['./inf-fue.component.css']
+  styleUrls: ['./inf-fue.component.css'],
 })
 export class InfFueComponent {
+  constructor(private fb: FormBuilder) {}
 
+  get idoferta() {
+    return this.formUserFue.get('idoferta') as FormControl;
+  }
+
+  get nom() {
+    return this.formUserFue.get('nom') as FormControl;
+  }
+
+  get tel() {
+    return this.formUserFue.get('tel') as FormControl;
+  }
+
+  get url() {
+    return this.formUserFue.get('url') as FormControl;
+  }
+
+  get obs1() {
+    return this.formUserFue.get('obs1') as FormControl;
+  }
+
+  get areresofer() {
+    return this.formUserFue.get('areresofer') as FormControl;
+  }
+
+  get nomresofer() {
+    return this.formUserFue.get('nomresofer') as FormControl;
+  }
+
+  get email() {
+    return this.formUserFue.get('email') as FormControl;
+  }
+
+  get obs2() {
+    return this.formUserFue.get('obs2') as FormControl;
+  }
+
+  formUserFue = this.fb.group({
+    idoferta: [{ value: '123456789', disabled: true }, Validators.required],
+    nom: ['', [Validators.required, Validators.maxLength(50)]],
+    tel: ['', [Validators.required, Validators.max(100000000000000)]],
+    url: ['', [Validators.required, Validators.maxLength(500)]],
+    obs1: ['', [Validators.required, Validators.maxLength(4000)]],
+    areresofer: ['', [Validators.required, Validators.maxLength(100)]],
+    nomresofer: ['', [Validators.required, Validators.maxLength(50)]],
+    email: ['', [Validators.required, Validators.maxLength(50)]],
+    obs2: ['', [Validators.required, Validators.maxLength(1000)]],
+  });
+
+  procesar() {
+    console.log(this.formUserFue.value);
+  }
 }
